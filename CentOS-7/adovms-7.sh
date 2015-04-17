aa5e21408547522b4df9ef410f66add16e81b955
magenx <designelo@gmail.com>
2015-04-17 13:13:23 +0100

    Update adovms-7.sh

--- CentOS-7/adovms-7.sh
+++ CentOS-7/adovms-7.sh
@@ -4,7 +4,7 @@
 #  Copyright (C) 2015 admin@magenx.com                               #
 #  All rights reserved.                                              #
 #====================================================================#
-
+# version
 ADOVMS_VER="3.0.11-3"
 
 # Repositories
@@ -13,6 +13,11 @@ REPO_EPEL="http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noar
 # Extra packages
 POSTFIX="http://repos.oostergo.net/6/postfix-3.0/postfix-3.0.1-1.el6.x86_64.rpm"
 EXTRA_PACKAGES="dovecot dovecot-pigeonhole opendkim git subversion clamav clamav-milter"
+
+# Configs
+POSTFIX_MAIN_CF="https://raw.githubusercontent.com/magenx/magenx-email-server/master/CentOS-7/main.cf"
+DOVECOT_CONF="https://raw.githubusercontent.com/magenx/magenx-email-server/master/CentOS-7/dovecot.conf"
+DOVECOT_SQL_CONF="https://raw.githubusercontent.com/magenx/magenx-email-server/master/CentOS-7/dovecot-sql.conf"
 
 # Simple colors
 RED="\e[31;40m"
@@ -249,7 +254,7 @@ echo -n "---> Download and configure ViMbAdmin 3? [y/n][n]:"
 read vmb_down
 if [ "${vmb_down}" == "y" ];then
      read -e -p "---> Edit your installation folder full path: " -i "/var/www/html/vmb" VMB_PATH
-	 echo
+        echo
         echo "  ViMbAdmin will be installed into:" 
 		GREENTXT ${VMB_PATH}
 		echo
@@ -295,7 +300,7 @@ echo -n "---> Download and configure ROUNDCUBE 1.1.x? [y/n][n]:"
 read rcb_down
 if [ "${rcb_down}" == "y" ];then
      read -e -p "---> Edit your installation folder full path: " -i "/var/www/html/rcb" RCB_PATH
-	 echo
+        echo
         echo "  ROUNDCUBE will be installed into:" 
 		GREENTXT ${RCB_PATH}
 		echo
@@ -341,7 +346,7 @@ read vmb_pass_gen
 if [ "${vmb_pass_gen}" == "y" ];then
    echo
      VMB_PASSGEN=$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1)
-     WHITETXT "ViMbAdmin database password: ${RED} ${VMB_PASSGEN}"
+     WHITETXT "ViMbAdmin database password: ${RED}${VMB_PASSGEN}"
      YELLOWTXT "!REMEMBER IT AND KEEP IT SAFE!"
 fi
 echo
@@ -360,7 +365,7 @@ exit
 EOMYSQL
 echo
 echo
-echo -n "---> SETUP ROUNDCUBE MySQL DATABASE AND USER 1.x? [y/n][n]:"
+echo -n "---> SETUP ROUNDCUBE MySQL DATABASE AND USER? [y/n][n]:"
 read rcb_sdb
 if [ "${rcb_sdb}" == "y" ];then
 echo
@@ -371,7 +376,7 @@ read rcb_pass_gen
 if [ "${rcb_pass_gen}" == "y" ];then
    echo
      RCB_PASSGEN=$(head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1)
-     WHITETXT "ROUNDCUBE database password: ${RED} ${RCB_PASSGEN}"
+     WHITETXT "ROUNDCUBE database password: ${RED}${RCB_PASSGEN}"
      YELLOWTXT "!REMEMBER IT AND KEEP IT SAFE!"
 fi
 echo
@@ -434,318 +439,32 @@ read -p "---> Enter your hostname : " VMB_MYHOSTNAME
 read -p "---> Enter your admin email : " VMB_ADMIN_MAIL
 read -e -p "---> Enter your ssl cert location: " -i "/etc/ssl/domain.crt"  VMB_SSL_CRT
 read -e -p "---> Enter your ssl key location: " -i "/etc/ssl/server.key"  VMB_SSL_KEY
-cat > /etc/postfix/main.cf <<END
-smtpd_banner = \${myhostname} ESMTP XBOX
 
-queue_run_delay = 1h
-minimal_backoff_time = 1h
-maximal_queue_lifetime = 3h
-maximal_backoff_time = 3h
-bounce_queue_lifetime = 3h
-
-
-biff = no
-append_dot_mydomain = no
-inet_interfaces = all
-dovecot_destination_recipient_limit = 1
-
-disable_vrfy_command = yes
-broken_sasl_auth_clients = no
-
-additional_config_dir = /etc/postfix/config
-
-smtpd_forbidden_commands = CONNECT GET POST
-smtpd_tls_cert_file = ${VMB_SSL_CRT}
-smtpd_tls_key_file = ${VMB_SSL_KEY}
-smtpd_tls_auth_only = yes
-smtpd_tls_security_level = may
-smtpd_tls_protocols=!SSLv2,!SSLv3
-smtpd_sasl_auth_enable = yes
-smtpd_sasl_type = dovecot
-smtpd_sasl_path = private/auth
-smtpd_helo_required = yes
-smtpd_reject_unlisted_sender = yes
-smtpd_reject_unlisted_recipient = yes
-smtpd_tls_session_cache_database = btree:\${data_directory}/smtpd_scache
-
-smtp_tls_session_cache_database = btree:\${data_directory}/smtp_scache
-smtp_tls_security_level = may
-smtp_tls_protocols=!SSLv2,!SSLv3
-smtp_always_send_ehlo = yes
-
-myhostname = ${VMB_MYHOSTNAME}
-mydomain = ${VMB_DOMAIN}
-mydestination = localhost
-mynetworks = 127.0.0.0/8 [::1]/128
-myorigin = \${mydomain}
-
-mailbox_size_limit = 0
-
-virtual_mailbox_domains = mysql:/etc/postfix/mysql/virtual-mailbox-domains.cf
-virtual_mailbox_maps = mysql:/etc/postfix/mysql/virtual-mailbox-maps.cf
-virtual_alias_maps = mysql:/etc/postfix/mysql/virtual-alias-maps.cf
-virtual_uid_maps = static:5000
-virtual_gid_maps = static:5000
-virtual_transport = dovecot
-
-smtpd_restriction_classes = rbl_cbl_abuseat_org,
-                            rbl_sbl_spamhaus_org,
-                            rbl_dul_ru, 
-                            rbl_spamcop,
-                            white_client_ip,
-                            black_client_ip,
-                            block_dsl,
-                            helo_access,
-                            white_client,
-                            black_client,
-                            mx_access
-
-rbl_cbl_abuseat_org  = reject_rbl_client cbl.abuseat.org
-rbl_dul_ru           = reject_rbl_client dul.ru
-rbl_sbl_spamhaus_org = reject_rbl_client sbl.spamhaus.org
-rbl_spamcop          = reject_rbl_client bl.spamcop.net
-
-white_client_ip      = check_client_access pcre:\$additional_config_dir/white_client_ip
-black_client_ip      = check_client_access pcre:\$additional_config_dir/black_client_ip
-white_client         = check_sender_access pcre:\$additional_config_dir/white_client
-black_client         = check_sender_access pcre:\$additional_config_dir/black_client
-block_dsl            = regexp:\$additional_config_dir/block_dsl
-helo_access          = check_helo_access pcre:\$additional_config_dir/helo_checks
-mx_access            = check_sender_mx_access cidr:\$additional_config_dir/mx_access
-
-smtpd_milters           = inet:127.0.0.1:8891
-non_smtpd_milters       = \${smtpd_milters}
-milter_default_action   = quarantine
-milter_protocol   = 6
-
-content_filter = scan:127.0.0.1:10025
-
-#notify_classes = bounce, delay, policy, protocol, resource, software
-#error_notice_recipient = ${VMB_ADMIN_MAIL}
-
-smtpd_client_restrictions = white_client_ip,
-                            black_client_ip,
-                            white_client,
-                            black_client,
-                            helo_access,
-                            block_dsl,
-                            rbl_dul_ru,
-                            rbl_sbl_spamhaus_org,
-                            rbl_spamcop,
-                            rbl_cbl_abuseat_org,
-                            permit_mynetworks,
-                            permit_sasl_authenticated,
-                            reject_unauth_destination,
-                            reject_unauth_pipelining,
-                            reject_unknown_address,
-                            reject_unknown_recipient_domain,
-                            reject_unknown_sender_domain
-
-smtpd_sender_restrictions = white_client,
-                            white_client_ip,
-                            black_client_ip,
-                            reject_unknown_recipient_domain,
-                            reject_unknown_sender_domain,
-                            reject_non_fqdn_recipient,
-                            reject_non_fqdn_sender,
-                            permit_sasl_authenticated,
-                            permit_mynetworks,
-                            mx_access,
-                            reject_unlisted_sender,
-                            reject_unauth_destination
-				
-smtpd_relay_restrictions = permit_mynetworks, permit_sasl_authenticated, reject_unauth_destination
-
-smtpd_recipient_restrictions = white_client,
-                               helo_access,
-                               reject_unknown_recipient_domain,
-                               reject_unknown_sender_domain,
-                               reject_non_fqdn_recipient,
-                               reject_non_fqdn_sender,
-                               reject_unauth_pipelining,
-                               permit_sasl_authenticated,
-                               permit_mynetworks,
-                               reject_unlisted_recipient,
-                               reject_unknown_address,
-                               reject_unauth_destination,
-                               reject_multi_recipient_bounce
-
-smtpd_data_restrictions = reject_unauth_pipelining,
-                          reject_multi_recipient_bounce
-						  
-sample_directory = /usr/share/doc/postfix-2.10.0/samples
-sendmail_path = /usr/sbin/sendmail
-setgid_group = postdrop
-command_directory = /usr/sbin
-manpage_directory = /usr/share/man
-daemon_directory = /usr/libexec/postfix
-newaliases_path = /usr/bin/newaliases
-mailq_path = /usr/bin/mailq
-queue_directory = /var/spool/postfix
-mail_owner = postfix
-data_directory = /var/lib/postfix
-END
-
-echo
-WHITETXT "Writing Postfix master.cf file"
-cat > /etc/postfix/master.cf <<END
-#
-# Postfix master process configuration file.  For details on the format
-# of the file, see the master(5) manual page (command: "man 5 master").
-#
-# Do not forget to execute "postfix reload" after editing this file.
-#
-# ==========================================================================
-# service type  private unpriv  chroot  wakeup  maxproc command + args
-#               (yes)   (yes)   (yes)   (never) (100)
-# ==========================================================================
-smtp      inet  n       -       n       -       -       smtpd
-smtps     inet  n       -       n       -       -       smtpd
-  -o smtpd_tls_wrappermode=yes
-  -o smtpd_sasl_auth_enable=yes
-  -o smtpd_client_restrictions=permit_sasl_authenticated,reject
-pickup    fifo  n       -       n       60      1       pickup
-cleanup   unix  n       -       n       -       0       cleanup
-qmgr      fifo  n       -       n       300     1       qmgr
-tlsmgr    unix  -       -       n       1000?   1       tlsmgr
-rewrite   unix  -       -       n       -       -       trivial-rewrite
-bounce    unix  -       -       n       -       0       bounce
-defer     unix  -       -       n       -       0       bounce
-trace     unix  -       -       n       -       0       bounce
-verify    unix  -       -       n       -       1       verify
-flush     unix  n       -       n       1000?   0       flush
-proxymap  unix  -       -       n       -       -       proxymap
-proxywrite unix -       -       n       -       1       proxymap
-smtp      unix  -       -       n       -       -       smtp
-# When relaying mail as backup MX, disable fallback_relay to avoid MX loops
-relay     unix  -       -       n       -       -       smtp
-        -o smtp_fallback_relay=
-showq     unix  n       -       n       -       -       showq
-error     unix  -       -       n       -       -       error
-retry     unix  -       -       n       -       -       error
-discard   unix  -       -       n       -       -       discard
-local     unix  -       n       n       -       -       local
-virtual   unix  -       n       n       -       -       virtual
-lmtp      unix  -       -       n       -       -       lmtp
-anvil     unix  -       -       n       -       1       anvil
-scache    unix  -       -       n       -       1       scache
-dovecot   unix  -       n       n       -       -       pipe
-  flags=DRhu user=vmail:vmail argv=/usr/libexec/dovecot/deliver -d \${recipient}
-scan      unix  -       -       n       -       16      smtp
-   -o smtp_data_done_timeout=1200
-   -o smtp_send_xforward_command=yes
-   -o disable_dns_lookups=yes
-   -o smtp_enforce_tls=no
-127.0.0.1:10026 inet n       -       n       -       16       smtpd
-   -o content_filter=
-   -o receive_override_options=no_unknown_recipient_checks,no_header_body_checks
-   -o local_recipient_maps=
-   -o relay_recipient_maps=
-   -o smtpd_restriction_classes=
-   -o smtpd_client_restrictions=
-   -o smtpd_helo_restrictions=
-   -o smtpd_sender_restrictions=
-   -o smtpd_recipient_restrictions=permit_mynetworks,reject
-   -o mynetworks_style=host
-   -o smtpd_authorized_xforward_hosts=127.0.0.0/8
-END
+wget -qO /etc/postfix/main.cf ${POSTFIX_MAIN_CF}
+sed -i "s/VMB_SSL_CRT/${VMB_SSL_CRT}/" /etc/postfix/main.cf
+sed -i "s/VMB_SSL_KEY/${VMB_SSL_KEY}/" /etc/postfix/main.cf
+sed -i "s/VMB_MYHOSTNAME/${VMB_MYHOSTNAME}/" /etc/postfix/main.cf
+sed -i "s/VMB_DOMAIN/${VMB_DOMAIN}/" /etc/postfix/main.cf
+sed -i "s/VMB_ADMIN_MAIL/${VMB_ADMIN_MAIL}/" /etc/postfix/main.cf
 
 echo
 WHITETXT "Writing Dovecot config file"
-cat > /etc/dovecot/dovecot.conf <<END
-auth_mechanisms = plain login
-disable_plaintext_auth = yes
-log_timestamp = "%Y-%m-%d %H:%M:%S "
-mail_location = maildir:/home/vmail/%d/%n
-mail_privileged_group = vmail
-
-ssl = required
-
-ssl_cert = <${VMB_SSL_CRT}
-ssl_key = <${VMB_SSL_KEY}
-ssl_protocols = !SSLv2 !SSLv3
-ssl_cipher_list = ALL:!LOW:!SSLv2:!EXP:!aNULL
-
-namespace {
-  inbox = yes
-  location =
-  prefix =
-  type = private
-}
-
-protocols = imap
-
-service imap-login {
-        service_count = 1
-        process_min_avail = 4
-        inet_listener imap {
-        port = 0
-        }
-}
-
-passdb {
-  driver = sql
-  args = /etc/dovecot/dovecot-sql.conf
-}
- 
-userdb {
-  driver = prefetch
-}
- 
-userdb {
-  driver = sql
-  args = /etc/dovecot/dovecot-sql.conf
-}
-
-service auth {
-  unix_listener /var/spool/postfix/private/auth {
-    group = postfix
-    mode = 0660
-    user = postfix
-  }
-unix_listener auth-master {
-    mode = 0600
-    user = vmail
-  }
-user = root
-}
-
-protocol lda {
-  auth_socket_path = /var/run/dovecot/auth-master
-  log_path = /home/vmail/dovecot-deliver.log
-  postmaster_address = ${VMB_ADMIN_MAIL}
-}
-
-## debug
-#auth_verbose=yes
-#auth_debug=yes
-#auth_debug_passwords=yes
-#mail_debug=yes
-#verbose_ssl=yes
-END
+wget -qO /etc/dovecot/dovecot.conf ${DOVECOT_CONF}
+sed -i "s/VMB_SSL_CRT/${VMB_SSL_CRT}/" /etc/dovecot/dovecot.conf
+sed -i "s/VMB_SSL_KEY/${VMB_SSL_KEY}/" /etc/dovecot/dovecot.conf
+sed -i "s/VMB_ADMIN_MAIL/${VMB_ADMIN_MAIL}/" /etc/dovecot/dovecot.conf
 
 echo
 WHITETXT "Writing Dovecot mysql connection file"
-cat > /etc/dovecot/dovecot-sql.conf <<END
-driver = mysql
-connect = host=${VMB_DB_HOST} dbname=${VMB_DB_NAME} user=${VMB_DB_USER_NAME} password=${VMB_PASSGEN}
-default_pass_scheme = SSHA512
-
-password_query = SELECT username as user, password as password, \
-        homedir AS userdb_home, maildir AS userdb_mail, \
-        concat('*:bytes=', quota) AS userdb_quota_rule, uid AS userdb_uid, gid AS userdb_gid \
-    FROM mailbox \
-        WHERE username = '%Lu' AND active = '1' \
-            AND ( access_restriction = 'ALL' OR LOCATE( '%Us', access_restriction ) > 0 )
-            
-user_query = SELECT homedir AS home, maildir AS mail, \
-        concat('*:bytes=', quota) as quota_rule, uid, gid \
-    FROM mailbox WHERE username = '%u'
-END
+wget -qO /etc/dovecot/dovecot-sql.conf ${DOVECOT_SQL_CONF}
+sed -i "s/VMB_DB_HOST/${VMB_DB_HOST}/" /etc/dovecot/dovecot-sql.conf
+sed -i "s/VMB_DB_NAME/${VMB_DB_NAME}/" /etc/dovecot/dovecot-sql.conf
+sed -i "s/VMB_DB_USER_NAME/${VMB_DB_USER_NAME}/" /etc/dovecot/dovecot-sql.conf
+sed -i "s/VMB_PASSGEN/${VMB_PASSGEN}/" /etc/dovecot/dovecot-sql.conf
 
 echo
-WHITETXT "Writing Postfix REJECT filters. Please uncomment/edit to your needs"
-WHITETXT "/etc/postfix/config/"
+WHITETXT "Writing Postfix PERMIT/REJECT filters. Please uncomment/edit to your needs"
+WHITETXT "at /etc/postfix/config/*"
 
 cat > /etc/postfix/config/black_client <<END
 #/^.*\@mail\.ru$/        REJECT        Your e-mail was banned!
@@ -806,98 +525,35 @@ cat > /etc/postfix/config/white_client_ip <<END
 END
 echo
 echo
-WHITETXT "Writing ClamSMTP filter config"
-cat > /etc/clamsmtpd.conf <<END
-# - Comments are a line that starts with a #
-# - All the options are found below with sample settings
-# The address to send scanned mail to.
-# This option is required unless TransparentProxy is enabled
-OutAddress: 127.0.0.1:10026
-# The maximum number of connection allowed at once.
-# Be sure that clamd can also handle this many connections
-#MaxConnections: 64
-# Amount of time (in seconds) to wait on network IO
-#TimeOut: 180
-# Keep Alives (ie: NOOP's to server)
-#KeepAlives: 0
-# Send XCLIENT commands to receiving server
-#XClient: off
-# Address to listen on (defaults to all local addresses on port 10025)
-Listen: 127.0.0.1:10025
-# The address clamd is listening on
-ClamAddress: /var/run/clamav/clamd.sock
-# A header to add to all scanned email
-Header: X-Virus-Scanned: ClamAV using ClamSMTP
-# Directory for temporary files
-TempDirectory: /tmp
-# What to do when we see a virus (use 'bounce' or 'pass' or 'drop'
-Action: drop
-# Whether or not to keep virus files
-Quarantine: off
-# Enable transparent proxy support
-TransparentProxy: off
-# User to switch to
-User: clamav
-# Virus actions: There's an option to run a script every time a virus is found.
-# !IMPORTANT! This can open a hole in your server's security big enough to drive
-# farm vehicles through. Be sure you know what you're doing. !IMPORTANT!
-VirusAction: /etc/postfix/valert.sh
+WHITETXT "Writing Clamav-milter config"
+cat > /etc/mail/clamav-milter.conf <<END
+MilterSocket inet:127.0.0.1:7357
+MilterSocketGroup clamav
+MilterSocketMode 660
+FixStaleSocket yes
+User clamilt
+AllowSupplementaryGroups yes
+ReadTimeout 120
+Foreground no
+PidFile /var/run/clamav-milter/clamav-milter.pid
+TemporaryDirectory /var/tmp
+ClamdSocket unix:/var/run/clamd.scan/clamd.sock
+LocalNet local
+LocalNet 127.0.0.1
+MaxFileSize 25M
+OnClean Accept
+OnInfected Reject
+OnFail Defer
+VirusAction /usr/local/bin/my_infected_message_handler
+LogFile /var/log/clamav-milter.log
+LogFileMaxSize 20M
+LogTime yes
+LogSyslog yes
+LogFacility LOG_MAIL
+LogRotate yes
+LogInfected Basic
 END
 echo
-WHITETXT "Writing ClamSMTP email alert script"
-cat > /etc/postfix/valert.sh <<END
-#!/bin/sh
-#
-# v0.1 (2009-04-09)
-#
-# VirusAction script to get virus alerts via email from ClamSMTP.
-#   Based on VirusAction script by Olivier Beyssac <ob@r14.freenix.org>
-#
-# Sends alerts:
-#	  1) to sender if domain part of sender address is local,
-#	otherwise to LOCAL recipients;
-#	  2)  in any case to explicitly specified recipients, e.g. postmaster
-#
-# Alexander Moisseev <moiseev@mezonplus.ru>
-#
-
-# Local domains (comma separated without spaces)
-MYDOMAINS=${VMB_DOMAIN}
-# Email addresses to send alerts to (comma separated without spaces)
-NOTIFY=${VMB_ADMIN_MAIL}
-
-
-MYDOM_RE=.+@$(echo "\${MYDOMAINS}" |  sed 's/,/$|.+@/g')$
-
-echo \${MYDOM_RE}
-if [ X$(echo \${SENDER} | egrep \${MYDOM_RE}) != "X" ];
-    then MAILTO=\${SENDER},\${NOTIFY}
-    else MAILTO=$(echo "\$RECIPIENTS" | egrep \${MYDOM_RE} | tr '\n' ',')\${NOTIFY}
-fi
-
-LINE="-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
-
-#(date "+%d-%m-%Y %H:%M:%S"
-(date
- echo
- echo "Virus name:     \${VIRUS}"
- echo "Sender:         \${SENDER}"
- echo "Recipient(s):   \${RECIPIENTS}"
- echo "SMTP client:    \${CLIENT}"
- echo "SMTP server:    \${SERVER}"
- echo "Remote client:  \${REMOTE}" | tr -d '\r'
- echo
- if [ "x\${EMAIL}" != "x" ] && [ -f \${EMAIL} ]
- then
-	echo "Quarantined to: \${EMAIL}"
-	echo
-	echo Headers follow:
-	echo \${LINE}
-	sed -n '1,/^.$/s/.$//gp' "\${EMAIL}"
-	echo \${LINE}
- fi
-) | cat -v | mail -s "[ VIRUS  ALERT ]: \${VIRUS} found on \${SERVER}" \${MAILTO}
-END
 echo
 WHITETXT "============================================================================="
 echo
