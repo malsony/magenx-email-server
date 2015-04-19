f174b08d244e4e00aec62923eb6a802ea9c2fbcc
magenx <designelo@gmail.com>
2015-04-19 23:41:35 +0100

    Create virus_alert.sh

---
+++ CentOS-7/virus_alert.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+
+SERVER=${HOSTNAME}
+ALERT=postmaster@domain.com
+
+# The following parameters are passed to the invoked program in this order:
+# virus name, queue id, sender, destination, subject, message id, message date.
+VIRUS=$1          # virus name
+QUEUE=$2          # queue id
+SENDER=$3         # sender
+DESTINATION=$4    # destination
+SUBJECT=$5        # subject
+MSGID=$6          # message id
+DATE=$7           # message date
+
+cat -v << EOF | mail -s "${SERVER}: an infected e-mail has been detected." ${ALERT}
+Signature   : ${VIRUS}
+Date        : ${DATE}
+Subject     : ${SUBJECT}
+
+Sender      : ${SENDER}
+Destination : ${DESTINATION}
+
+Queue       : ${QUEUE}
+Message ID  : ${MSGID}
+
+Message was rejected. Further action is not required.
+EOF
